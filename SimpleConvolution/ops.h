24a47824e6ef69a21f133cc1c435162b6f12160d
bbbbang <ghkddlf95@gmail.com>
2022-01-19 18:23:31 +0900

    remove vectorize

--- SimpleConvolution/ops.h
+++ SimpleConvolution/ops.h
@@ -13,14 +13,186 @@ std::chrono::microseconds Relu(T* inputData, int height, int width, int channel)
 template <typename T>
 std::chrono::microseconds Concat(T* inputData, T* outputData, int height, int width, int channel);
 
+
+// vectorize
+
+//template <typename T>
+//std::chrono::microseconds ZeroPadding(T* inputData, T* outputData, int height, int width, int channel, int padding)
+//{
+//	std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+//
+//	int vectorizeCount = 10;
+//	int repeat = height / vectorizeCount;
+//	repeat *= repeat;
+//
+//	int inputIndex;
+//	int outputIndex;
+//
+//	int inputArea = height * width;
+//	int outputWidth = (height + padding * 2);
+//	int outputArea = outputWidth * outputWidth;
+//
+//	for (int ch = 0; ch < channel; ++ch)
+//	{
+//#pragma region vectorize
+//
+//		int i = 0;
+//		int j = 0;
+//		repeat = height / vectorizeCount;
+//		repeat *= repeat;
+//		while (repeat-- > 0)
+//		{
+//			outputIndex = ch * outputArea + (j + 1) * outputWidth + (i + 1);
+//			inputIndex = ch * inputArea + j * width + i;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//			outputIndex += outputWidth;
+//			inputIndex += width;
+//
+//			outputData[outputIndex + 0] = inputData[inputIndex + 0];
+//			outputData[outputIndex + 1] = inputData[inputIndex + 1];
+//			outputData[outputIndex + 2] = inputData[inputIndex + 2];
+//			outputData[outputIndex + 3] = inputData[inputIndex + 3];
+//			outputData[outputIndex + 4] = inputData[inputIndex + 4];
+//			outputData[outputIndex + 5] = inputData[inputIndex + 5];
+//			outputData[outputIndex + 6] = inputData[inputIndex + 6];
+//			outputData[outputIndex + 7] = inputData[inputIndex + 7];
+//			outputData[outputIndex + 8] = inputData[inputIndex + 8];
+//			outputData[outputIndex + 9] = inputData[inputIndex + 9];
+//
+//			i += vectorizeCount;
+//			if (i >= height)
+//			{
+//				i = 0;
+//				j += vectorizeCount;
+//			}
+//		}
+//#pragma endregion
+//	}
+//
+//	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+//	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+//}
+
+
+
 template <typename T>
 std::chrono::microseconds ZeroPadding(T* inputData, T* outputData, int height, int width, int channel, int padding)
 {
 	std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
-
-	int vectorizeCount = 10;
-	int repeat = height / vectorizeCount;
-	repeat *= repeat;
 
 	int inputIndex;
 	int outputIndex;
@@ -31,158 +203,21 @@ std::chrono::microseconds ZeroPadding(T* inputData, T* outputData, int height, i
 
 	for (int ch = 0; ch < channel; ++ch)
 	{
-		#pragma region vectorize
-
-		int i = 0;
-		int j = 0;
-		repeat = height / vectorizeCount;
-		repeat *= repeat;
-		while (repeat-- > 0)
+		for (int row = 0; row < height; ++row)
 		{
-			outputIndex = ch * outputArea + (j + 1) * outputWidth + (i + 1);
-			inputIndex = ch * inputArea + j * width + i;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-			outputIndex += outputWidth;
-			inputIndex += width;
-
-			outputData[outputIndex + 0] = inputData[inputIndex + 0];
-			outputData[outputIndex + 1] = inputData[inputIndex + 1];
-			outputData[outputIndex + 2] = inputData[inputIndex + 2];
-			outputData[outputIndex + 3] = inputData[inputIndex + 3];
-			outputData[outputIndex + 4] = inputData[inputIndex + 4];
-			outputData[outputIndex + 5] = inputData[inputIndex + 5];
-			outputData[outputIndex + 6] = inputData[inputIndex + 6];
-			outputData[outputIndex + 7] = inputData[inputIndex + 7];
-			outputData[outputIndex + 8] = inputData[inputIndex + 8];
-			outputData[outputIndex + 9] = inputData[inputIndex + 9];
-
-			i += vectorizeCount;
-			if (i >= height)
+			for (int col = 0; col < width; ++col)
 			{
-				i = 0;
-				j += vectorizeCount;
+				outputData[ch * outputArea + (row+1) * width + (col+1)] = inputData[ch * inputArea + row * width + col];
 			}
 		}
-		#pragma endregion
 	}
 
 	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
 	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 }
+
+
+
 
 template <typename T>
 std::chrono::microseconds Add(T* inputData, T* outputData, int height, int width, int channel)
