bd6eeb8e6fe8874cbeb321c3aea4849f0dc7de20
bbbbang <ghkddlf95@gmail.com>
2022-01-20 18:12:50 +0900

    add some ops
    zero concat, max pool, resize 2x

--- SimpleConvolution/ops.h
+++ SimpleConvolution/ops.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <chrono>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
 
 template <typename T>
 std::chrono::microseconds ZeroPadding(T* inputData, T* outputData, int height, int width, int channel, int padding);
@@ -203,13 +206,23 @@ std::chrono::microseconds ZeroPadding(T* inputData, T* outputData, int height, i
 
 	for (int ch = 0; ch < channel; ++ch)
 	{
+		outputData += outputWidth;
 		for (int row = 0; row < height; ++row)
 		{
+			++outputData;
 			for (int col = 0; col < width; ++col)
 			{
-				outputData[ch * outputArea + (row+1) * width + (col+1)] = inputData[ch * inputArea + row * width + col];
+				T val = *inputData;
+				*outputData = val;
+
+				//outputData[ch * outputArea + (row+1) * outputWidth + (col+1)] = inputData[ch * inputArea + row * width + col];
+
+				++inputData;
+				++outputData;
 			}
+			++outputData;
 		}
+		outputData += outputWidth;
 	}
 
 	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
@@ -222,10 +235,24 @@ std::chrono::microseconds ZeroPadding(T* inputData, T* outputData, int height, i
 template <typename T>
 std::chrono::microseconds Add(T* inputData, T* outputData, int height, int width, int channel)
 {
+	std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+
 	for (int i = 0; i < height * width * channel; ++i)
 	{
-		*outputData++ = *inputData++ + *outputData;
+		//*outputData++ = *inputData++ + *outputData;
+
+		T out = *outputData;
+		T in = *inputData;
+
+		out += in;
+		*outputData = out;
+
+		++outputData;
+		++inputData;
 	}
+
+	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 }
 
 template <typename T>
@@ -240,9 +267,117 @@ std::chrono::microseconds Relu(T* inputData, int height, int width, int channel)
 template <typename T>
 std::chrono::microseconds Concat(T* inputData, T* outputData, int height, int width, int channel)
 {
+	std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+
 	int size = height * width * channel;
 	for (int i = size; i < size * 2; ++i)
 	{
 		inputData[i] = outputData[i - size];
 	}
+
+	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+}
+
+
+template <typename T>
+std::chrono::microseconds ZeroConcat(T* inputData, T* outputData, int height, int width, int channel);
+template <typename T>
+std::chrono::microseconds ZeroConcat(T* inputData, T* outputData, int height, int width, int channel)
+{
+	std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+
+	int size = height * width * channel;
+	for (int i = size; i < size * 2; ++i)
+	{
+		inputData[i] = 0;
+	}
+
+	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+}
+
+template <typename T>
+std::chrono::microseconds MaxPool(T* inputData, T* outputData, int height, int width, int channel, int kernel, int stride, int padding);
+
+template <typename T>
+std::chrono::microseconds MaxPool(T* inputData, T* outputData, int height, int width, int channel, int kernel, int stride, int padding)
+{
+	std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+
+	int padHeight = height + padding * 2;
+	int padWidth = width + padding * 2;
+	int padArea = padWidth * padHeight;
+
+	int outputHeight = (padHeight - kernel) / stride + 1;
+	int outputWidth = (padWidth - kernel) / stride + 1;
+	int outputArea = outputHeight * outputWidth;
+
+	for (int ch = 0; ch < channel; ++ch)
+	{
+		for (int row = 0; row < height; row+=stride)
+		{
+			for (int col = 0; col < width; col += stride)
+			{
+				T val1 = *inputData;
+				T val2 = *(inputData + 1);
+				T val3 = *(inputData + width);
+				T val4 = *(inputData + width + 1);
+
+				T max = std::max({ val1, val2, val3, val4 });
+				*outputData = max;
+
+				inputData+= stride;
+				++outputData;
+			}
+		}
+	}
+
+	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+}
+
+
+template <typename T>
+std::chrono::microseconds Resize(T* inputData, T* outputData, int height, int width, int channel, float scale);
+
+template <typename T>
+std::chrono::microseconds Resize(T* inputData, T* outputData, int height, int width, int channel, float scale)
+{
+	std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+	// nearest neighbor
+
+	int outputHeight = height * scale;
+	int outputWidth = width * scale;
+	int outputArea = outputHeight * outputWidth;
+
+	int area = height * width;
+
+	for (int i = 0; i < channel * outputArea; ++i)
+	{
+		outputData[i] = 0;
+	}
+
+	for (int ch = 0; ch < channel; ++ch)
+	{
+		for (int row = 0; row < height; ++row)
+		{
+			for (int col = 0; col < width; ++col)
+			{
+				T val = *inputData;
+
+				*outputData = val;
+				*(outputData + 1) = val;
+				*(outputData + outputWidth) = val;
+				*(outputData + outputWidth + 1) = val;
+
+				++inputData;
+				outputData+=2;
+			}
+		}
+
+	}
+
+	std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 }
