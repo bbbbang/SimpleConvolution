e8c801a687b1b5686f245839318fc19f316ce751
bbbbang <ghkddlf95@gmail.com>
2022-02-11 18:00:45 +0900

    Update utils.h

--- SimpleConvolution/utils.h
+++ SimpleConvolution/utils.h
@@ -133,6 +133,95 @@ void ReadWeights_debug(std::string fileName, std::unordered_map<std::string, Lay
 }
 
 
+
+void ReadWeights_txt(std::string fileName, std::unordered_map<std::string, Layer>& map)
+{
+	std::ifstream weightFile(fileName);
+
+	if (weightFile.is_open())
+	{
+		float data;
+
+		int inChannel;
+		int outChannel;
+		int kernel;
+		int stride;
+		int padding;
+		int group;
+
+		std::vector<float> weights;
+		std::vector<float> bias;
+		int weightSize;
+
+		int layersNum = 0;
+		int weightNum = 0;
+		int biasNum = 0;
+
+		while (weightFile)
+		{
+			weightFile >> data;
+
+			if ((int)data == 9000)
+			{
+				if (layersNum > 0)
+				{
+					map[std::to_string(layersNum)] = Layer{ inChannel, outChannel, kernel, stride, padding, group, weightSize, weights, bias };
+				}
+				weights.clear();
+				bias.clear();
+				weightNum = 0;
+				biasNum = 0;
+				layersNum += 1;
+
+				weightFile >> data;
+				inChannel = (int)data;
+				weightFile >> data;
+				outChannel = (int)data;
+				weightFile >> data;
+				kernel = (int)data;
+				weightFile >> data;
+				stride = (int)data;
+				weightFile >> data;
+				padding = (int)data;
+				weightFile >> data;
+				group = (int)data;
+
+				if (group == 1)
+				{
+					weightSize = inChannel * outChannel * kernel * kernel;
+				}
+				else
+				{
+					weightSize = inChannel * 1 * kernel * kernel;
+				}
+			}
+			else
+			{
+				if (weightNum < weightSize)
+				{
+					weights.push_back(data);
+					weightNum += 1;
+				}
+				else if (weightNum >= weightSize && biasNum < outChannel)
+				{
+					bias.push_back(data);
+					biasNum += 1;
+				}
+			}
+		}
+		map[std::to_string(layersNum)] = Layer{ inChannel, outChannel, kernel, stride, padding, group, weightSize, weights, bias };
+		weightFile.close();
+	}
+	else
+	{
+		std::cout << "no file" << std::endl;
+	}
+
+}
+
+
+
+
 void CopyTensor(Tensor* dst, Tensor* src)
 {
 	dst->height = src->height;
