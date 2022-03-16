4983c2c24f26544426214bf37a130e1be4246233
bbbbang <ghkddlf95@gmail.com>
2022-03-16 19:18:13 +0900

    Update convolution_c.h

--- convolution_c.h
+++ convolution_c.h
@@ -141,8 +141,6 @@ void _Convolution2D_Depthwise_k3_s1(Tensor* tensor, float* weight, float* bias, 
 	float* tempInputData2 = tempTensor + padWidth;
 	float* tempInputData3 = tempInputData2 + padWidth;
 
-	float* val = data;
-
 	for (int inCh = 0; inCh < inChannel; ++inCh)
 	{
 		int kernelIndex = inCh * 9;
@@ -155,14 +153,14 @@ void _Convolution2D_Depthwise_k3_s1(Tensor* tensor, float* weight, float* bias, 
 		{
 			for (int col = 0; col < width; ++col)
 			{
-				*val += *(tempInputData1)*weightVal_1 + *(tempInputData1 + 1) * weightVal_2 + *(tempInputData1 + 2) * weightVal_3 +
+				*data = *(tempInputData1)*weightVal_1 + *(tempInputData1 + 1) * weightVal_2 + *(tempInputData1 + 2) * weightVal_3 +
 					*(tempInputData2)*weightVal_4 + *(tempInputData2 + 1) * weightVal_5 + *(tempInputData2 + 2) * weightVal_6 +
 					*(tempInputData3)*weightVal_7 + *(tempInputData3 + 1) * weightVal_8 + *(tempInputData3 + 2) * weightVal_9 + *bias;
 
 				++tempInputData1;
 				++tempInputData2;
 				++tempInputData3;
-				++val;
+				++data;
 			}
 			tempInputData1 += 2;
 			tempInputData2 += 2;
@@ -210,9 +208,7 @@ void _Convolution2D_Depthwise_k3_s2(Tensor* tensor, float* weight, float* bias, 
 	float* tempInputData1 = tempTensor;
 	float* tempInputData2 = tempTensor + padWidth;
 	float* tempInputData3 = tempTensor + padWidth + padWidth;
-	float* tempOutputData = data;
 
-	float* val = data;
 	for (int inCh = 0; inCh < inChannel; ++inCh)
 	{
 		int kernelIndex = inCh * 9;
@@ -225,14 +221,14 @@ void _Convolution2D_Depthwise_k3_s2(Tensor* tensor, float* weight, float* bias, 
 		{
 			for (int col = 0; col < width; col += stride)
 			{
-				*val += *(tempInputData1)*weightVal_1 + *(tempInputData1 + 1) * weightVal_2 + *(tempInputData1 + 2) * weightVal_3 +
+				*data = *(tempInputData1)*weightVal_1 + *(tempInputData1 + 1) * weightVal_2 + *(tempInputData1 + 2) * weightVal_3 +
 					*(tempInputData2)*weightVal_4 + *(tempInputData2 + 1) * weightVal_5 + *(tempInputData2 + 2) * weightVal_6 +
 					*(tempInputData3)*weightVal_7 + *(tempInputData3 + 1) * weightVal_8 + *(tempInputData3 + 2) * weightVal_9 + *bias;
 
 				tempInputData1 += stride;
 				tempInputData2 += stride;
 				tempInputData3 += stride;
-				++val;
+				++data;
 			}
 			tempInputData1 += padWidth + 2;
 			tempInputData2 += padWidth + 2;
@@ -264,21 +260,261 @@ void _Convolution2D_Pointwise_k1_s1(Tensor* tensor, float* weight, float* bias, 
 	int area = height * width;
 
 	float* data = tensor->data;
-	//float* tempData = (float*)malloc(sizeof(float) * inChannel * area);
 	memcpy(tempTensor, data, sizeof(float) * inChannel * area);
 	memset(data, 0, sizeof(float) * outChannel * area);
 
 
-	for (int outCh = 0; outCh < outChannel; ++outCh)
-	{
-		for (int i = 0; i < area; ++i)
-		{
-			float val = 0;
-			for (int inCh = 0; inCh < inChannel; ++inCh)
-			{
-				val += 
+	Transpose(tensor);
 
+	if (area % 10 == 0)
+	{
+		for (int outCh = 0; outCh < outChannel; ++outCh)
+		{
+			float* d = tempTensor;
+			for (int i = 0; i < area; i += 10)
+			{
+				float val1 = 0; float val2 = 0; float val3 = 0;
+				float val4 = 0; float val5 = 0; float val6 = 0;
+				float val7 = 0; float val8 = 0; float val9 = 0; float val10 = 0;
+				float* w = weight;
+				float dd[10];
+				float ww[10];
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val1 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val1 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val2 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val2 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val3 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val3 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val4 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val4 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val5 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val5 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val6 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val6 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val7 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val7 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val8 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val8 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val9 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val9 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val10 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val10 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+					d += 8;
+					w += 8;
+				}
+				float b = *bias;
+				float val[10] = { val1 + b,val2 + b,val3 + b,val4 + b,val5 + b,val6 + b,val7 + b,val8 + b,val9 + b,val10 + b };
+				memcpy(data, val, sizeof(float) * 10);
+				data += 10;
 			}
+			weight += inChannel;
+			++bias;
+		}
+	}
+	else
+	{
+		for (int outCh = 0; outCh < outChannel; ++outCh)
+		{
+			float* d = tempTensor;
+			for (int i = 0; i < area; i += 5)
+			{
+				float val1 = 0;
+				float val2 = 0;
+				float val3 = 0;
+				float val4 = 0;
+				float val5 = 0;
+				float* w = weight;
+
+				float dd[8];
+				float ww[8];
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val1 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val1 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val2 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val2 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val3 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val3 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val4 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val4 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				w = weight;
+				for (int inCh = 0; inCh < inChannel; inCh += 8)
+				{
+					//memcpy(dd, d, sizeof(4) * 8);
+					//memcpy(ww, w, sizeof(4) * 8);
+					//val5 += dd[0] * ww[0] + dd[1] * ww[1] + dd[2] * ww[2] + dd[3] * ww[3] +
+					//	dd[4] * ww[4] + dd[5] * ww[5] + dd[6] * ww[6] + dd[7] * ww[7];
+
+					val5 += *(d) * *(w)+*(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+						*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+					d += 8;
+					w += 8;
+				}
+				float b = *bias;
+				float val[5] = { val1 + b,val2 + b,val3 + b,val4 + b,val5 + b };
+				memcpy(data, val, sizeof(float) * 5);
+				data += 5;
+			}
+			weight += inChannel;
+			++bias;
 		}
 	}
 
@@ -288,39 +524,173 @@ void _Convolution2D_Pointwise_k1_s1(Tensor* tensor, float* weight, float* bias, 
 
 
 
+	//for (int outCh = 0; outCh < outChannel; ++outCh)
+	//{
+	//	float *d = tempTensor;
+	//	//for (int i = 0; i < area; ++i)
+	//	//{
+	//	//	float val = 0;
+	//	//	float* w = weight;
 
-	float* o = data;
-	for (int outCh = 0; outCh < outChannel; ++outCh)
-	{
-		float* d = o;
-		float* v = tempTensor;
 
-		float weightVal = *weight;
-		for (int i = 0; i < area; ++i)
-		{
-			(*o) += *v * weightVal + *bias;
-			++o;
-			++v;
-		}
-		for (int inCh = 1; inCh < inChannel; ++inCh)
-		{
-			o = d;
-			float weightVal = *weight;
+	//	//	//for (int inCh = 0; inCh < inChannel; ++inCh)
+	//	//	//{
+	//	//	//	val += *(d) * *(w);
 
-			for (int i = 0; i < area; ++i)
-			{
-				(*o) += *v * weightVal;
-				++o;
-				++v;
-			}
-			++weight;
-		}
-		++bias;
-	}
+	//	//	//	++d;
+	//	//	//	++w;
+	//	//	//}
+
+	//	//	for (int inCh = 0; inCh < inChannel; inCh+=8)
+	//	//	{
+	//	//		val += *(d) * *(w) + *(d+1) * *(w+1) + *(d+2) * *(w+2) + *(d+3) * *(w+3) +
+	//	//			*(d+4) * *(w+4) + *(d+5) * *(w+5) + *(d+6) * *(w+6) + *(d+7) * *(w+7);
+
+	//	//		d+=8;
+	//	//		w+=8;
+	//	//	}
+
+	//	//	*data = val + *bias;
+	//	//	++data;
+	//	//}
+
+	//	for (int i = 0; i < area; i += 5)
+	//	{
+	//		float val1 = 0;
+	//		float val2 = 0;
+	//		float val3 = 0;
+	//		float val4 = 0;
+	//		float val5 = 0;
+	//		float* w = weight;
+
+	//		for (int inCh = 0; inCh < inChannel; inCh += 8)
+	//		{
+	//			val1 += *(d) * *(w)+ *(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+	//				*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+	//			d += 8;
+	//			w += 8;
+	//		}
+	//		w = weight;
+
+	//		for (int inCh = 0; inCh < inChannel; inCh += 8)
+	//		{
+	//			val2 += *(d) * *(w)+ *(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+	//				*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+	//			d += 8;
+	//			w += 8;
+	//		}
+	//		w = weight;
+	//		for (int inCh = 0; inCh < inChannel; inCh += 8)
+	//		{
+	//			val3 += *(d) * *(w)+ *(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+	//				*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+	//			d += 8;
+	//			w += 8;
+	//		}
+	//		w = weight;
+	//		for (int inCh = 0; inCh < inChannel; inCh += 8)
+	//		{
+	//			val4 += *(d) * *(w)+ *(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+	//				*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+	//			d += 8;
+	//			w += 8;
+	//		}
+	//		w = weight;
+	//		for (int inCh = 0; inCh < inChannel; inCh += 8)
+	//		{
+	//			val5 += *(d) * *(w)+ *(d + 1) * *(w + 1) + *(d + 2) * *(w + 2) + *(d + 3) * *(w + 3) +
+	//				*(d + 4) * *(w + 4) + *(d + 5) * *(w + 5) + *(d + 6) * *(w + 6) + *(d + 7) * *(w + 7);
+
+	//			d += 8;
+	//			w += 8;
+	//		}
+	//		float b = *bias;
+	//		float val[5] = { val1+b,val2 + b,val3 + b,val4 + b,val5 + b };
+	//		memset(data, val, sizeof(float) * 5);
+	//		//*(data) = val1 + b;
+	//		//*(data+1) = val2 + b;
+	//		//*(data+2) = val3 + b;
+	//		//*(data+3) = val4 + b;
+	//		//*(data+4) = val5 + b;
+
+	//		data += 5;
+	//		//*data = val + *bias;
+	//		//++data;
+	//	}
+
+
+
+	//	weight += inChannel;
+	//	++bias;
+	//}
+
+
+
+	Transpose(tensor);
+
+
+	//float* o = data;
+	//for (int outCh = 0; outCh < outChannel; ++outCh)
+	//{
+	//	float* d = o;
+	//	float* v = tempTensor;
+
+	//	float weightVal = *weight;
+	//	for (int inCh = 0; inCh < inChannel; ++inCh)
+	//	{
+	//		o = d;
+	//		float weightVal = *weight;
+
+	//		for (int i = 0; i < area; ++i)
+	//		{
+	//			(*o) += *v * weightVal;
+	//			++o;
+	//			++v;
+	//		}
+	//		++weight;
+	//	}
+	//	++bias;
+	//}
+
+
+
+
+
+
+	//float* o = data;
+	//for (int outCh = 0; outCh < outChannel; ++outCh)
+	//{
+	//	float* d = o;
+	//	float* v = tempTensor;
+
+	//	float weightVal = *weight;
+	//	for (int i = 0; i < area; ++i)
+	//	{
+	//		(*o) += *v * weightVal + *bias;
+	//		++o;
+	//		++v;
+	//	}
+	//	for (int inCh = 1; inCh < inChannel; ++inCh)
+	//	{
+	//		o = d;
+	//		float weightVal = *weight;
+
+	//		for (int i = 0; i < area; ++i)
+	//		{
+	//			(*o) += *v * weightVal;
+	//			++o;
+	//			++v;
+	//		}
+	//		++weight;
+	//	}
+	//	++bias;
+	//}
 
 	tensor->channel = outChannel;
-	//free(tempData);
-	//tempData = NULL;
 }
 
 
@@ -345,58 +715,7 @@ void _Convolution2D_Pointwise_k1_s1(Tensor* tensor, float* weight, float* bias, 
 
 
 
-void _Transpose(Tensor* tensor)
-{
-	int height = tensor->height;
-	int width = tensor->width;
-	int channel = tensor->channel;
 
-	float* data = tensor->data;
-	//float* transTensor = (float*)malloc(sizeof(float) * height * width * channel);
-	memcpy(tempTensor, data, sizeof(float) * height * width * channel);
-
-	int idx = 0;
-	for (int ch = 0; ch < channel; ++ch)
-	{
-		for (int row = 0; row < height; ++row)
-		{
-			for (int col = 0; col < width; ++col)
-			{
-				data[idx] = tempTensor[row * width * channel + col * channel + ch];
-				++idx;
-			}
-		}
-	}
-	//free(transTensor);
-	//transTensor = NULL;
-}
-
-
-void Transpose(Tensor* tensor)
-{
-	int height = tensor->height;
-	int width = tensor->width;
-	int channel = tensor->channel;
-
-	float* data = tensor->data;
-	//float* transTensor = (float*)malloc(sizeof(float) * height * width * channel);
-	memcpy(tempTensor, data, sizeof(float) * height * width * channel);
-
-	int idx = 0;
-	for (int row = 0; row < height; ++row)
-	{
-		for (int col = 0; col < width; ++col)
-		{
-			for (int ch = 0; ch < channel; ++ch)
-			{
-				data[idx] = tempTensor[ch * height * width + row * width + col];
-				++idx;
-			}
-		}
-	}
-	//free(transTensor);
-	//transTensor = NULL;
-}
 
 
 void Mult(Tensor* tensor, Tensor* _tensor)
